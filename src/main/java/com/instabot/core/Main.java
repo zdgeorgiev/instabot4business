95b4f68d89234d719ca99fe680d772f89a837bb9
zdgeorgiev <zddavko@gmail.com>
2018-12-02 19:10:30 +0200

    fixed

--- src/main/java/com/instabot/core/Main.java
+++ src/main/java/com/instabot/core/Main.java
@@ -14,7 +14,7 @@ import java.io.IOException;
 import java.util.*;
 import java.util.stream.Collectors;
 
-public class Kur {
+public class Main {
 
 	private static List<String> followers = new ArrayList<>();
 
